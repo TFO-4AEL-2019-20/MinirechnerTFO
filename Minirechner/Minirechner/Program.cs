418b55c82d4c15b0f077a98c7b50f986141fe8b9
Martinverdorfer <62984623+Martinverdorfer@users.noreply.github.com>
2020-04-23 10:51:41 +0200

    Master

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -7,14 +7,17 @@ namespace Minirechner
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-           
+
             Console.WriteLine("Zahl1:");
-            int Zahl1= Convert.ToInt32(Console.ReadLine());
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Zahl2:");
             int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
 
         }
     }
