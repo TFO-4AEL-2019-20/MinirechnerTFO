88601d11f02d17a2f75a0de341e88e555e8ff758
Martinverdorfer <62984623+Martinverdorfer@users.noreply.github.com>
2020-04-23 10:43:40 +0200

    1.Commit

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Zahl1:");
+            int Zahl1= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Zahl2:");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
